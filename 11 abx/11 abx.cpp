2788a74b2069f351f2a75f115caf7c6cdbf5f23b
ihsan <ihsanmpm@gmail.com>
2017-12-16 18:43:30 +0530

    Add project files.

---
+++ 11 abx/11 abx.cpp
@@ -0,0 +1,16 @@
+// 11 abx.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int x;
+	x = a, b;
+	std::cout << x;
+    return 0;
+}
+
